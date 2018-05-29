f7b386d9f6b112718ad2fbc5dbdfe5e0abe3a791
sofiane merouane <sofiane.merouane@dr11.cnrs.fr>
2018-05-29 15:53:28 +0200

    Revert "ajout fonction testRevert"
    This reverts commit 3de73abd8e5700d9e0e16c56ca159bf4a38c7da6.

--- app/src/app.c
+++ app/src/app.c
@@ -24,8 +24,4 @@ int liba_goodbye(){
 	return 0;
 }
 
-int testRevert(){
-	return 1;
-}
-
 /* EOF */
---
+++ liba/src/liba.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "liba/liba.h"
+
+void liba_hello() {
+  fprintf(stdout, "%s\n", "hello from liba");
+}
+
+void libabyebye() {}
+/* EOF */
